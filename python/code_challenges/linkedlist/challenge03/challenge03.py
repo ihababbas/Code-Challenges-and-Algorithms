a6da10778b7ac54a98f62bc8891ca1cfde6fe627
ihababbas <ihababbas26@gmail.com>
2022-10-26 12:12:36 +0300

    docstring

--- python/code_challenges/linkedlist/challenge03/challenge03.py
+++ python/code_challenges/linkedlist/challenge03/challenge03.py
@@ -3,13 +3,18 @@ class Node:
     def __init__(self,value):
         self.value = value
         self.next = None
-
+    '''
+    this class will be the construtor
+    '''
 
 class LinkedList:
     def __init__(self):
         self.head = None
-
+    
     def append(self, node):
+        '''
+         this method will add the node that created to the linkedlist of this class
+         '''
         if self.head is None:
             self.head = node
         else:
@@ -17,26 +22,28 @@ class LinkedList:
             while current.next is not None:
                 current = current.next
             current.next = node
-
+    
     def printAll(self):
+        '''
+         this method willtake all added nodes from the linkied list and print them as a list
+        '''
         list =[]
         current = self.head
         while current is not None:
                 list.append(current.value)
                 current = current.next
         print(list)
+    
     def deleteNode(self, n):
+        '''
+        Given a linked list and an integer N, the task is to delete the Nth node from the end of the given linked list.
+        '''
         first = self.head
         second = self.head
         
         for i in range(n):
-             
-            # If count of nodes in the
-            # given list is less than 'n'
             if(second.next == None):
                 
-                # If index = n then
-                # delete the head node
                 if(i == n - 1):
                     self.head = self.head.next
                 return self.head
@@ -51,9 +58,21 @@ class LinkedList:
         
 
 
+'''
+#to run the code
 
-           
-
+linkedList1 = LinkedList()
+node1 = Node("A")
+linkedList1.append(node1)
+node2 = Node("B")
+linkedList1.append(node2)
+node3 = Node("C")
+linkedList1.append(node3)
+node4 = Node("D")
+linkedList1.append(node4)
+linkedList1.deleteNode(2)
+linkedList1.printAll()        
+'''
 
 
 
